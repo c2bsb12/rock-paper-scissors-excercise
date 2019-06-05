3406e39964f4ebe5538d8cdc00e5f7a83d2e4e6d
Sheridan <sdb358@stern.nyu.edu>
2019-06-05 18:56:21 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Rock, Paper, Scissors, Shoot!") 
+
+#capture input
+user_choice = input("Choose your warrior: 'rock', 'paper' or 'scissors' (without the quotes)")
+
+print("**********************")
+print("USER CHOICE:", user_choice)
+
+
+#validate input
+
+#generate computer selection
+
+#determine the winner
+
+#display final outputs/outcomes
