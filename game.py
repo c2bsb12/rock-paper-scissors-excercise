a0a40d74b793d063978441393ca1fb0f845fc8ca
Sheridan <sdb358@stern.nyu.edu>
2019-06-05 19:26:29 -0400

    generate computer choice

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!") 
 
 #capture input
-user_choice = input("Choose your warrior: 'rock', 'paper' or 'scissors' (without the quotes)")
+user_choice = input("Choose your warrior: 'rock', 'paper' or 'scissors' (without the quotes):")
 
-print("__________________________")
-print("USER CHOICE:FAILURE", user_choice)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("USER CHOICE:", user_choice)
 
 
 #validate input
@@ -14,14 +16,18 @@ print("USER CHOICE:FAILURE", user_choice)
 
 
 if user_choice in ["rock", "paper", "scissors"]:
-    print("VALID")
+    pass
 else:
     print("INVALID SELECTION. PLEASE TRY AGAIN")
     exit()
 
 #generate computer selection
 
+computer_choice = random.choice (["rock", "paper", "scissors"])
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("GENERATING...")
+print("COMPUTER CHOICE:", computer_choice)
 
 #determine the winner
 
